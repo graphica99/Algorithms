e811cd0986d353879fda16cca36cdb9b0c9d011c
mends albert <mendsalbert@gmail.com>
2021-05-23 10:38:00 +0100

    added new changes

--- Trees/BinarySearchTree.js
+++ Trees/BinarySearchTree.js
@@ -35,12 +35,31 @@ class BinarySearchTree {
       }
     }
   }
+
+  search(value) {
+    var current = this.root;
+    while (current != null) {
+      if (value === current.data) return current.data;
+      if (value < current.data) {
+        console.log("less");
+        current = current.leftNode;
+      } else {
+        current = current.rightNode;
+      }
+    }
+    return null;
+  }
 }
 
 var tree = new BinarySearchTree();
 tree.create(10);
 tree.create(5);
-// tree.create(13);
+tree.create(13);
+tree.create(11);
+tree.create(2);
+tree.create(16);
+tree.create(7);
+console.log(tree.search());
 
 //      10
 //   5     13
